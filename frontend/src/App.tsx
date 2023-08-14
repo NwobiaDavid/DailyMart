5dcbd6ddf11d030bfe35c80744d6be0ae2fa6b7d
NwobiaDavid <dnwobia@gmail.com>
2023-08-14 11:53:47 +0100

    initialized repo

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+
+  return (
+    <>
+     <header> DAILY MART </header>
+     <main> </main>
+     <footer>all rights reserved</footer>
+    </>
+  )
+}
+
+export default App
