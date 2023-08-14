34959d0ac410a7e0534964e606cbb644cdae2ce3
NwobiaDavid <dnwobia@gmail.com>
2023-08-14 18:02:18 +0100

    iterations

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,14 @@
+import cors from 'cors';
 import express, { Request, Response } from 'express'; 
 import { sampleProducts } from './data';
 
 const app = express();
+app.use(
+    cors({
+        credentials: true,
+        origin: ['http://localhost:5173']
+    })
+)
 const PORT = 3000 || process.env.PORT;
 
 app.get('/api/products', (req: Request, res:Response)=>{
