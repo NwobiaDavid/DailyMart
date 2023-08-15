397772a1a4ebcc2008a88c48150688225400e0a8
NwobiaDavid <dnwobia@gmail.com>
2023-08-15 14:37:50 +0100

    added dark and light mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,31 @@
 import { Outlet } from "react-router-dom";
+import { Store } from "./Store";
+import {useContext, useEffect} from 'react'
+import { Button } from "react-bootstrap";
 
 
 function App() {
+
+  const {state:{mode}, dispatch} = useContext(Store);
+
+  useEffect(() => {
+    document.body.setAttribute('data-bs-theme', mode)
+  }, [mode])
+
+  const switchModeHandler = () =>{
+    dispatch({type: 'SWITCH_MODE'})
+  }
 
   return (
     <div >
      <header className="flex px-3 bg-slate-950 text-white justify-between py-[1rem] items-center">
           <div>DAILY MART </div>
-        <div className="flex">
+        <div className="flex items-center">
           <a href="/cart" className="nav-link">Cart</a>
           <a href="/signin" className="ml-2 nav-link">signin</a>
+          <Button variant={mode} onClick={switchModeHandler}>
+            <i className={mode === 'light' ? 'fa fa-sun text-white' : 'fas fa-moon'}></i>
+          </Button>
         </div>
       </header>
      <main className="flex items-center justify-center ">
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
-} from "react-router-dom";
-import App from './App.tsx'
-import './index.css'
+} from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
 import Homepage from './pages/Homepage.tsx';
 import ProductPage from './pages/ProductPage.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
+import { StoreProvider } from './Store.tsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} element={<Homepage />} />
-      <Route path='product/:slug' element={<ProductPage />} />
+      <Route path="product/:slug" element={<ProductPage />} />
       {/* <Route path="dashboard" element={<Dashboard />} /> */}
       {/* ... etc. */}
     </Route>
@@ -31,11 +31,13 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HelmetProvider>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-    </HelmetProvider>
-  </React.StrictMode>,
-)
+    <StoreProvider>
+      <HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </HelmetProvider>
+    </StoreProvider>
+  </React.StrictMode>
+);
