2885ff89f57b2cf3d3b830ee58d02441d3a89252
NwobiaDavid <dnwobia@gmail.com>
2023-08-17 02:20:55 +0100

    implemented paypal payment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import { productRouter } from './routers/productRouter';
 import { seedRouter } from './routers/seedRouter';
 import { userRouter } from './routers/userRouter';
 import { orderRouter } from './routers/orderRouter';
+import { keyRouter } from './routers/keyRouter';
 
 dotenv.config();
 const app = express();
@@ -38,6 +39,7 @@ app.use('/api/products', productRouter);
 app.use('/api/seed', seedRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
+app.use('/api/keys', keyRouter);
 
 app.listen(PORT, ()=>{
     console.log(`listening on port ${PORT}...`)
