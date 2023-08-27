3509bda3d4f5a06e996abe2c34f0f9e90c201e02
NwobiaDavid <dnwobia@gmail.com>
2023-08-27 18:27:56 +0100

    updated

--- backend/src/routers/keyRouter.ts
+++ backend/src/routers/keyRouter.ts
@@ -6,3 +6,8 @@
  keyRouter.get('/paypal', (req,res)=>{
     res.json({clientId: process.env.PAYPAL_CLIENT_ID || 'sb'})
  })
+
+  // /api/keys/flutterwave
+  keyRouter.get('/paypal', (req,res)=>{
+   res.json({clientId: process.env.FLUTTERWAVE_PK || 'sb'})
+})
